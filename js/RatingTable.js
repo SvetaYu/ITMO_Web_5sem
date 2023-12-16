08ff5210aa40c3df775bf75d9724dad7702e7f35
SvetaYu <svetlanavolga2003@gmail.com>
2023-12-16 04:38:39 +0300

    lab5 refactoring

--- js/RatingTable.js
+++ js/RatingTable.js
@@ -1,5 +1,5 @@
 document.getElementById('form').addEventListener('submit', function (event) {
-    event.preventDefault(); // Предотвращаем перезагрузку страницы
+    event.preventDefault();
 
     let form = document.getElementById("form");
     const table = document.getElementById('table');
@@ -38,43 +38,3 @@ document.getElementById('form').addEventListener('submit', function (event) {
         cell.appendChild(textNode);
     }
 });
-
-// document.addEventListener('DOMContentLoaded', function () {
-//     const table = document.getElementById('table');
-//     restoreTable(table);
-// );
-//
-//     document.getElementById('form').addEventListener('submit', function (event) {
-//         event.preventDefault(); // Предотвращаем перезагрузку страницы
-//
-//         const form = document.getElementById("form");
-//         const table = document.getElementById('table');
-//
-//         const name = document.getElementById('name').value;
-//         const rating = document.getElementById('rating').value;
-//         const comment = document.getElementById('comment').value;
-//
-//         generateTable(name, rating, comment, table);
-//         form.reset();
-//     );
-//
-//         function restoreTable(table) {
-//             let storedTable = localStorage.getItem("tableData");
-//             if (storedTable) {
-//                 table.innerHTML = storedTable;
-//             }
-//
-//
-//             function generateTable(name, rating, comment, table) {
-//                 const row = table.insertRow();
-//                 addCell(row, name);
-//                 addCell(row, rating);
-//                 addCell(row, comment);
-//
-//                 localStorage.setItem("tableData", table.innerHTML);
-//
-//
-//                 function addCell(row, text) {
-//                     const cell = row.insertCell();
-//                     const textNode = document.createTextNode(text);
-//                     cell.appendChild(textNode);
