9929689b9e2aa93d1b8d7e9a69e882fbf3f0d351
SvetaYu <svetlanavolga2003@gmail.com>
2023-12-17 14:07:21 +0300

    lab5 fix restore rating table

--- js/RatingTable.js
+++ js/RatingTable.js
@@ -1,40 +1,41 @@
-document.getElementById('form').addEventListener('submit', function (event) {
-    event.preventDefault();
-
+document.addEventListener("DOMContentLoaded", function () {
     let form = document.getElementById("form");
-    const table = document.getElementById('table');
+    const dataTable = document.getElementById('table');
 
-
-    const name = document.getElementById('name').value;
-    const rating = document.getElementById('rating').value;
-    const comment = document.getElementById('comment').value;
-
+    function saveTable() {
+        localStorage.setItem("tableData", dataTable.innerHTML);
+    }
 
     function restoreTable() {
         let storedTable = localStorage.getItem("tableData");
         if (storedTable) {
-            table.innerHTML = storedTable;
+            dataTable.innerHTML = storedTable;
         }
     }
 
     restoreTable();
 
-    generateTable(name, rating, comment, table)
+    form.onsubmit = function (event) {
+        event.preventDefault();
 
+        const name = document.getElementById('name').value;
+        const rating = document.getElementById('rating').value;
+        const comment = document.getElementById('comment').value;
 
-    function generateTable(name, rating, comment, table) {
+        let row = dataTable.insertRow();
 
-        const row = table.insertRow();
         addCell(row, name);
         addCell(row, rating);
         addCell(row, comment);
 
-        form.reset();
-        localStorage.setItem("tableData", table.innerHTML);
+        function addCell(row, text) {
+            const cell = row.insertCell();
+            cell.textContent = text;
+        }
+
+
+            form.reset();
+            saveTable();
+        };
     }
-    function addCell(row, text) {
-        const cell = row.insertCell();
-        const textNode = document.createTextNode(text);
-        cell.appendChild(textNode);
-    }
-});
+);
